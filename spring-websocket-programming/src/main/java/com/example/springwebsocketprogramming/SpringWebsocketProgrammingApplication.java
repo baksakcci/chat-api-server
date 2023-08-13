259fb1b64b178806c4356f24f972dd39f930680e
baksakcci <pshsh910@gmail.com>
2023-08-13 13:57:55 +0900

    init: spring websocket project

---
+++ spring-websocket-programming/src/main/java/com/example/springwebsocketprogramming/SpringWebsocketProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocketprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketProgrammingApplication.class, args);
+	}
+
+}
